b4ff6eff31da71d914bb32272051ecb34e12f228
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 14:28:33 +0700

    add generator

--- blocks/blocks_tracking.js
+++ blocks/blocks_tracking.js
@@ -1,15 +1,19 @@
-Blockly.Blocks['ky_033_tracking_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-033 TRACKING");
-    this.appendValueInput("KY_033_TRACKING_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_033_tracking_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-033 TRACKING");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_033_TRACKING_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_033_TRACKING_VALUE"),
+          "KY_033_TRACKING_VALUE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_tracking.js
+++ blocks/generators_tracking.js
@@ -1,6 +1,21 @@
-Blockly.JavaScript['ky_033_tracking_block'] = function(block) {
-  var value_ky_033_tracking_pin = Blockly.JavaScript.valueToCode(block, 'KY_033_TRACKING_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_033_tracking_block"] = function(block) {
+    var text_ky_033_tracking_pin = block.getFieldValue("KY_033_TRACKING_PIN");
+    var variable_ky_033_tracking_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_033_TRACKING_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+
+	#VARIABLE
+	int KY_033_TRACKING_PIN=${text_ky_033_tracking_pin};
+	#END
+
+	#SETUP
+	pinMode(KY_033_TRACKING_PIN,INPUT);
+	#END
+
+	${variable_ky_033_tracking_value}=digitalRead(KY_033_TRACKING_PIN);
+    `;
+    return code;
+  };
